0c4c8dcec6aa5b8a85acb379cb62a8b021a61e1b
Victor Parente <66752969+parvic@users.noreply.github.com>
2021-06-18 16:57:02 -0300

    Revert "Feature/auth"

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,33 +1,24 @@
 import { useState } from "react";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
-import { useSession, signIn, signOut } from "next-auth/client";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const [session] = useSession();
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  return session ? (
-    <button
-      className={styles.buttonContainer}
-      type="button"
-      onClick={() => signOut()}
-    >
+  return isLoggedIn ? (
+    <button className={styles.buttonContainer} type="button">
       <FaGithub color="#04d361" />
-      <span>{session.user.name}</span>
+      <span>Victor Parente</span>
       <FiX className={styles.closeIcon} />
     </button>
-  ) : (
-    <button
-      className={styles.buttonContainer}
-      type="button"
-      onClick={() => signIn("GitHub")}
-    >
-      <FaGithub color="#eba417" />
-      <span>Sign in with GitHub</span>
-      <FiX className={styles.closeIcon} />
-    </button>
+    ) : (
+    <button className={styles.buttonContainer} type="button">
+     <FaGithub color="#eba417" />
+     <span>Sign in with GitHub</span>
+     <FiX className={styles.closeIcon} />
+   </button>
   );
 }
