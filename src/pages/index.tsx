dc9adac6c8c54d54b99b6f19abe109d6f3879d68
victorbruno <victorbrunoparente@gmail.com>
2021-06-18 17:01:46 -0300

    Fix[home]: Change GetServerProps to GetStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
+
 import { Header } from "../components/Header";
 import { SubscribeButton } from "../components/SubscribeButton";
 
@@ -33,7 +34,7 @@ export default function Home({ product }: HomeProps) {
             Get access to all the publications <br />
             <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="" />
       </main>
@@ -41,7 +42,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1J0zjIB70pYtdXBlftvKsoPl", {
     expand: ["product"],
   });
@@ -58,5 +59,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24,
   };
 };
