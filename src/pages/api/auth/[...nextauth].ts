f56e0760ae803bb9bbbb186469d5794d2e54ba45
victorbruno <victorbrunoparente@gmail.com>
2021-06-29 01:50:18 -0300

    Feat[Auth+DB]: Store user email on login
    Using Fauna DB, the app will get user info from GitHub and store user email into data base.

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
 import NextAuth from 'next-auth'
+import { signIn } from 'next-auth/client'
 import Providers from 'next-auth/providers'
+import { query as q } from "faunadb";
+
+import { fauna } from "services/fauna";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -11,6 +15,41 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      console.log(user);
+      const { email } = user;
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+        
+        return true;
+      } catch (error) {
+        console.log(error)
+        return false;
+      }
+    }
+  }
 
   // A database is optional, but required to persist accounts in a database
   //database: process.env.DATABASE_URL, //Since we are not using a DB, we don't need to use this in our application
